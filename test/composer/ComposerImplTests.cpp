8c12b6b765ac95306b3d919165ce95b01d5b5daf
oskar <siorek.oskar1@gmail.com>
2019-06-06 00:22:26 +0200

    NOTE-REMOVAL
    tests for removing

--- test/composer/ComposerImplTests.cpp
+++ test/composer/ComposerImplTests.cpp
@@ -19,13 +19,13 @@ TEST(composer, composeSite)
     const char *messages = composer->composeSite(msgs);
 
     cout << messages;
-    string messages3 = std::string(messages);
+    string actualHtml = std::string(messages);
     //then
-    string html = "<html>\n\n<metaname=\"viewport\"content=\"width=device-width\">\n<metaname=\"viewport\"content=\"width=device-width,initial-scale=1\">\n\n<linkrel=\"stylesheet\"href=\"materialize.min.css\">\n<body>\n<divclass=\"container\">\n<h3>Notes</h3>\n<table>\n<thead>\n<tr>\n<th>Note</th>\n<th>Actions</th>\n</tr>\n</thead>\n\n<tbody>\n<tr>\n<td>test1</td>\n<td><ahref=\"/messages\"method=\"DELETE\"class=\"waves-effectwaves-lightbtnred\">DELETE</a></td></tr>\n</tbody><tbody>\n<tr>\n<td>test2</td>\n<td><ahref=\"/messages\"method=\"DELETE\"class=\"waves-effectwaves-lightbtnred\">DELETE</a></td></tr>\n</tbody></table>\n</ul>\n<br><br>\n<formaction=\"/messages\"method=\"post\">\n<b>Newnote:</b><inputtype=\"text\"name=\"note\">\n<inputclass=\"waves-lightbtngreen\"type=\"submit\"value=\"Addnote\">\n</form>\n</div>\n</body>\n</html>";
+    string expectedHtml = "<html>\n\n<metaname=\"viewport\"content=\"width=device-width\">\n<metaname=\"viewport\"content=\"width=device-width,initial-scale=1\">\n\n<linkrel=\"stylesheet\"href=\"materialize.min.css\">\n<body>\n<divclass=\"container\">\n<h3>Notes</h3>\n<table>\n<thead>\n<tr>\n<th>Note</th>\n<th>Actions</th>\n</tr>\n</thead>\n\n<tr><formaction=\"/messages/delete\"method=\"post\"><inputtype=\"hidden\"name=\"messageId\"value=0\"/><td>test1</td><td><inputtype=\"submit\"class=\"waves-effectwaves-lightbtnred\"value=\"DELETE\"></td></form></tr><tr><formaction=\"/messages/delete\"method=\"post\"><inputtype=\"hidden\"name=\"messageId\"value=1\"/><td>test2</td><td><inputtype=\"submit\"class=\"waves-effectwaves-lightbtnred\"value=\"DELETE\"></td></form></tr></table>\n</ul>\n<br><br>\n<formaction=\"/messages\"method=\"post\">\n<b>Newnote:</b><inputtype=\"text\"name=\"note\">\n<inputclass=\"waves-lightbtngreen\"type=\"submit\"value=\"Addnote\">\n</form>\n</div>\n</body>\n</html>";
 
-    std::string::iterator end_pos2 = std::remove(messages3.begin(), messages3.end(), ' ');
-    messages3.erase(end_pos2, messages3.end());
-    ASSERT_EQ(messages3, html);
+    std::string::iterator end_pos2 = std::remove(actualHtml.begin(), actualHtml.end(), ' ');
+    actualHtml.erase(end_pos2, actualHtml.end());
+    ASSERT_EQ(expectedHtml, actualHtml);
 }
 
 int main(int argc, char **argv)
