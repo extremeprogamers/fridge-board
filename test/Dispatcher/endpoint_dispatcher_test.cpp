802c642773e24e3672584ac58ad0e3989ce0507e
oskar <siorek.oskar1@gmail.com>
2019-06-05 21:04:35 +0200

    DISPATCHER
    Added tests regarding remove, post, composeSite

---
+++ test/Dispatcher/endpoint_dispatcher_test.cpp
@@ -0,0 +1,87 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "gmock/gmock-actions.h"
+
+#include "MessagesRepository.h"
+#include "Composer.h"
+#include "EndpointDispatcher.h"
+
+using namespace testing;
+
+class MockMessagesRepository : public MessagesRepository
+{
+public:
+    MOCK_METHOD1(add, bool(string));
+    MOCK_METHOD1(remove, bool(string));
+    MOCK_METHOD0(getAll, vector<string>(void));
+    MOCK_METHOD0(clear, bool(void));
+};
+
+class MockComposer : public Composer{
+public:
+    MOCK_METHOD1(composeSite, const char *(vector<string>));
+    MOCK_METHOD1(composeGetMessages, string(vector<string>));
+    MOCK_METHOD1(composeMessage, string(string));
+    MOCK_METHOD2(composeAdd, const char *(string, bool));
+    MOCK_METHOD2(composeRemoved, const char *(string, bool));
+};
+
+TEST(dispatcher, getMsgs)
+{
+    //given
+    MockMessagesRepository mockRepository;
+    MockComposer mockComposer;
+    EndpointDispatcher *dispatcher = new EndpointDispatcher(&mockRepository, &mockComposer);
+
+    vector<string> mockMessages;
+    string t1 = "test";
+    mockMessages.push_back(t1);
+
+    EXPECT_CALL(mockComposer, composeSite(mockMessages))
+        .Times(1)
+        .WillOnce(Return("<html><body>Wiadomosci: 1, 2</body></html>"));
+
+    EXPECT_CALL(mockRepository, getAll())
+        .Times(1)
+        .WillOnce(Return(mockMessages));
+
+    //when
+    const char *messages = dispatcher->getMsgs();
+
+    //then
+    ASSERT_EQ(messages, "<html><body>Wiadomosci: 1, 2</body></html>");
+}
+
+TEST(dispatcher, postMsg)
+{
+    //given
+    MockMessagesRepository mockRepository;
+    MockComposer mockComposer;
+    EndpointDispatcher *dispatcher = new EndpointDispatcher(&mockRepository, &mockComposer);
+
+    EXPECT_CALL(mockRepository, add("test"))
+        .Times(1);
+
+    //when
+    dispatcher->postMsg("test");
+}
+
+TEST(dispatcher, removeMsg)
+{
+    //given
+    MockMessagesRepository mockRepository;
+    MockComposer mockComposer;
+    EndpointDispatcher *dispatcher = new EndpointDispatcher(&mockRepository, &mockComposer);
+
+    EXPECT_CALL(mockRepository, remove("test"))
+        .Times(1);
+
+    //when
+    dispatcher->deleteMsg(5);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
