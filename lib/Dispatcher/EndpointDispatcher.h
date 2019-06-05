13bc1d952fd591c05ffdc245fb4ea4a7330bfffc
oskar <siorek.oskar1@gmail.com>
2019-06-05 21:06:59 +0200

    DISPATCHER
    Added functionality

---
+++ lib/Dispatcher/EndpointDispatcher.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef FRIDGE1_ENDPOINT_DISPATCHER_H
+#define FRIDGE1_ENDPOINT_DISPATCHER_H
+
+#include <string>
+#include <list>
+#include "MessagesRepository.h"
+#include "Composer.h"
+
+using namespace std;
+
+class EndpointDispatcher {
+private:
+    MessagesRepository *repository;
+    Composer *composer;
+
+public:
+    EndpointDispatcher(MessagesRepository *db, Composer *comp) {
+        this->repository = db;
+        this->composer = comp;
+    }
+    const char *getMsgs() {
+        vector<string> messages = this->repository->getAll();
+        return this->composer->composeSite(messages);
+    }
+
+    void postMsg(string msg) {
+        repository->add(msg);
+    }
+
+    void *deleteMsg(int id) {
+        repository->remove("test");
+    }
+};
+
+#endif
